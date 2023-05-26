a57f6366957bc1ed743dadf9e1c2a2daff9e5ba6
FatlumGjinofci <fatlum@thesocialplus.com>
2023-05-26 17:30:50 +0200

    wip...

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,11 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [\App\Http\Controllers\LoginController::class, 'submit']);
-Route::post('/login/verify', [\App\Http\Controllers\LoginController::class, 'verify']);
+Route::post('/login', [LoginController::class, 'submit']);
+Route::post('/login/verify', [LoginController::class, 'verify']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    
+
+    Route::get('/driver', [DriverController::class, 'show']);
+    Route::post('/driver', [DriverController::class, 'update']);
+
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
